559cec7376425ebc3837e9a47a4d52d5b67a5ad7
David Freilich <dfreilich@pivotal.io>
2020-01-14 08:46:54 -0500

    Add Server Streaming call to the greet client

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/dfreilich/grpc-samples/greet/greetpb"
@@ -13,6 +14,11 @@ import (
 
 const address = "localhost"
 const port = "50051"
+
+var dFGreeting = &greetpb.Greeting{
+	FirstName: "David",
+	LastName:  "Freilich",
+}
 
 func main() {
 	log.Println("Starting client")
@@ -32,21 +38,47 @@ func run() error {
 	c := greetpb.NewGreetServiceClient(cc)
 	fmt.Println("Created client")
 
-	return doUnary(c)
+	// if err := doUnary(c); err != nil {
+	// 	return errors.Wrap(err, "failed to do unary RPC call")
+	// }
+
+	return doServerStreaming(c)
 }
 
 func doUnary(c greetpb.GreetServiceClient) error {
 	fmt.Println("Starting to do Unary RPC...")
 	req := &greetpb.GreetRequest{
-		Greeting: &greetpb.Greeting{
-			FirstName: "David",
-			LastName:  "Freilich",
-		},
+		Greeting: dFGreeting,
 	}
 	res, err := c.Greet(context.Background(), req)
 	if err != nil {
 		return errors.Wrap(err, "error while calling Greet rpc")
 	}
 	fmt.Printf("Response from Greet: %v\n", res)
+	return nil
+}
+
+func doServerStreaming(c greetpb.GreetServiceClient) error {
+	fmt.Println("Starting a Server Streaming RPC")
+
+	req := &greetpb.GreetManyTimesRequest{Greeting: dFGreeting}
+
+	resStream, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		return errors.Wrap(err, "error while calling GreetManyTimes RPC")
+	}
+	for {
+		msg, err := resStream.Recv()
+		if err == io.EOF {
+			// Stream has ended
+			break
+		}
+		if err != nil {
+			return errors.Wrap(err, "error while reading stream")
+		}
+
+		fmt.Printf("Response from GreetManyTimes: %v\n", msg.GetResult())
+	}
+
 	return nil
 }
