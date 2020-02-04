8b2db6c324fb918004fa781ead63b8268010c593
David Freilich <dfreilich@pivotal.io>
2020-02-04 18:50:36 -0500

    Make channel before create go routine

--- blog/server/server.go
+++ blog/server/server.go
@@ -62,15 +62,15 @@ func run() error {
 	})
 	reflection.Register(s)
 
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
 	go func() {
 		fmt.Println("Starting Server...")
 		if err := s.Serve(lis); err != nil {
 			log.Fatalf("failed to serve: %v", err)
 		}
 	}()
-
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, os.Interrupt)
 
 	<-ch
 	fmt.Println("Stopping the server")
